fef77e97ee997a47947e98c42fa203a37eda34e9
nia <niadev76@gmail.com>
2011-10-18 21:56:06 -0400

    Applied minor fix for Real-Time Activity/State Monitor Screen
    Signed-off-by: nia <niadev76@gmail.com>

--- html/mhvtl_fifo.php
+++ html/mhvtl_fifo.php
@@ -5,12 +5,14 @@ $fifo = fopen("/var/tmp/mhvtl", "r+");
 if ($fifo) {
     stream_set_blocking($fifo, false);
   echo "<b><FONT COLOR=white>$dtt </FONT></b><br>";
-    } else {
-  echo "<b><FONT COLOR=white>Pipe: </FONT><FONT COLOR=red>Offline</FONT></b>";
-    }
+
 $data = fgets($fifo);
 if ($data === false) echo "<FONT COLOR=green>Ready ...</FONT><br>";
 sleep(0);
 echo "<FONT COLOR=#736AFF>$data</FONT><br>";
-pclose($fifo);
+
+    } else {
+  echo "<b><FONT COLOR=white>Pipe: </FONT><FONT COLOR=red>Offline</FONT></b>";
+    }
+fclose($fifo);
 ?>
