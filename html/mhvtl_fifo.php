1e3340c67e5ce7667a0d5300d88ccc12280b23cc
nia <niadev76@gmail.com>
2012-02-28 23:09:22 -0500

    minor fixes
    Signed-off-by: nia <niadev76@gmail.com>

--- html/mhvtl_fifo.php
+++ html/mhvtl_fifo.php
@@ -1,16 +1,15 @@
 <?php
-$mhvtl_ver = shell_exec('sudo -u root -S vtlcmd -V| cut -d "-" -f1,3| cut -d ":" -f2| cut -d " " -f2| cut -d "-" -f1');
+$mhvtl_ver = shell_exec('sudo -u root -S vtlcmd -V| cut -d "-" -f1,3| cut -d ":" -f2| cut -d " " -f2');
 $dtt = `date`;
 $adjperm = `sudo -u root -S chmod 777 /var/tmp/mhvtl`;
 $fifo = fopen("/var/tmp/mhvtl", "r+");
 if ($fifo) {
     stream_set_blocking($fifo, false);
   echo "<b><FONT COLOR=white>$dtt </FONT></b><br>";
-
 $data = fgets($fifo);
-if ($data === false) echo "<br><FONT COLOR=#736AFF>MHVTL Release $mhvtl_ver </FONT><FONT COLOR=#6FFF00>[ Ready ]</FONT>";
+if ($data === false) echo "<br><FONT COLOR=#736AFF>MHVTL Release $mhvtl_ver </FONT><FONT COLOR=#6FFF00><br>Ready ...</FONT>";
 sleep(0);
-echo "<FONT COLOR=#736AFF><br>$data</FONT><br>";
+echo "<FONT COLOR=#736AFF ><br>$data</FONT><br>";
     } else {
   echo "<b><FONT COLOR=white>Pipe: </FONT><FONT COLOR=red>Offline</FONT><FONT COLOR=white> or option</FONT><FONT COLOR=yellow> disabled</FONT></b>";
     }
