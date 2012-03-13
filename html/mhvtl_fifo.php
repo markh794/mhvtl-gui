55b5e6078b888273a241668ed9092536f8f41f8a
nia <niadev76@gmail.com>
2012-03-13 00:54:18 -0400

    Removed fifo pipe display by default
    Signed-off-by: nia <niadev76@gmail.com>

--- html/mhvtl_fifo.php
+++ html/mhvtl_fifo.php
@@ -7,12 +7,12 @@ if ($fifo) {
     stream_set_blocking($fifo, false);
   echo "<b><FONT COLOR=white>$dtt</FONT></b><br>";
 $data = fgets($fifo);
-if ($data === false) echo "<br><FONT COLOR=#00FFFF>MHVTL</FONT><br><FONT COLOR=#FF00FF>Release $mhvtl_ver </FONT><FONT COLOR=#6FFF00><br>Ready ...</FONT>";
+if ($data === false) echo "<br><FONT COLOR=#00FFFF>MHVTL</FONT><br><FONT COLOR=#736AFF>Release $mhvtl_ver </FONT><FONT COLOR= #FFFF00 > - fifo pipe enabled</FONT><FONT COLOR=#6FFF00><br>Ready ...</FONT>";
 sleep(0);
 $a= strstr($data, '-', false);
 echo "<FONT COLOR=#736AFF ><br>$a</FONT><br>";
     } else {
-  echo "<b><FONT COLOR=white>Pipe: </FONT><FONT COLOR=red>Offline</FONT><FONT COLOR=white> or option</FONT><FONT COLOR=yellow> disabled</FONT></b>";
-    }
+echo "<b><FONT COLOR=white>$dtt</FONT></b><br><br><FONT COLOR=#00FFFF>MHVTL</FONT><br><FONT COLOR=#736AFF>Release $mhvtl_ver </FONT><FONT COLOR=#6FFF00></FONT>";
+   }
 fclose($fifo);
 ?>
