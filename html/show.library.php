01ea38ab52d4c9778eb02f25a6c57b061cef58c7
nia <niadev76@gmail.com>
2012-08-04 00:14:18 -0400

    minor fixes
    Signed-off-by: nia <niadev76@gmail.com>

--- html/show.library.php
+++ html/show.library.php
@@ -19,15 +19,13 @@ echo "<pre><b>Show Library :</b></pre>";
 <TABLE BORDER='4' CELLSPACING='4' CELLPADDING='4' bgcolor='#000000' <FONT COLOR='#FFFFFF'></FONT>
 <TR>
 <TD>
-
-
+<div style="overflow:auto;height:250px;width:400px;">
 <?php
 $VAR = $_REQUEST['libid'];
 $output = `sudo -u root -S cat /etc/mhvtl/library_contents.$VAR| grep -v ^#`;
 echo "<pre><FONT COLOR=#FFFF00>$output</FONT></pre>";
 ?>
-
-
+</div>
 </TR>
 </TD>
 </TABLE>
