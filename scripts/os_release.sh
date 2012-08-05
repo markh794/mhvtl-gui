85f6564aa6414691d93c4babded1d651564b7789
niadev67 <niadev67@gmail.com>
2012-08-05 13:27:45 -0400

    Added few more minor fixes

--- scripts/os_release.sh
+++ scripts/os_release.sh
@@ -4,7 +4,7 @@
 OS=`uname -s`
 REV=`uname -r`
 MACH=`uname -m`
-HOST=`hostname`
+HOST=`hostname -s`
 
 GetVersionFromFile()
 {
@@ -74,7 +74,7 @@ if [ "${OS}" = "Linux" ] ; then
 		DIST="${DIST}[`cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//`]"
 	fi
 	
-	OSSTR="$HOST - ${OS} ${DIST} ${REV}(${PSUEDONAME} ${KERNEL} ${MACH})"
+	OSSTR="$HOST - ${OS} ${DIST} ${REV}(${PSUEDONAME} ${KERNEL})"
 
 fi
 echo ${OSSTR}
