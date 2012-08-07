d639c8a070881364e13d42264ca16dfb2e5739dc
niadev67 <niadev67@gmail.com>
2012-08-07 12:15:30 -0400

    Fixed handling properly of deleting media under mounted directories

--- scripts/remove_library.sh
+++ scripts/remove_library.sh
@@ -18,23 +18,38 @@ echo "<FONT COLOR=#FFA500><b>Removing Library "$VAR1" + Drives ...</b></FONT>"
 sed "/Library: $VAR1/,/Library:/{//p;d;}" /etc/mhvtl/device.conf | grep -v ^"Library: $VAR1" >/tmp/devicetmp.conf
 cp -f /tmp/devicetmp.conf /etc/mhvtl/device.conf
 rm -f /tmp/devicetmp.conf
+echo "<FONT COLOR=#FFA500><b>STATUS:$?</b></FONT>"
 echo "<pre><FONT COLOR=#FF0000><b>*** Library : $VAR1 Removed </b></FONT></pre>"
 
 if [ "$VAR2" = "YES" ]; then
-echo "<FONT COLOR=#FF00FF><b>Removing all existing tape media ...</b></FONT>"
+echo "<FONT COLOR=#FF0000><b>Removing all existing tape media ...</b></FONT>"
 if [ -d /opt/mhvtl/$VAR1 ] ; then
+
+if mountpoint -q -d /opt/mhvtl/$VAR1 ; then
+rm -Rf /opt/mhvtl/$VAR1/*
+echo "<FONT COLOR=#FFA500><b>STATUS:$?</b></FONT>"
+echo "<pre><FONT COLOR=#FFFF00><b>Removed /opt/mhvtl/$VAR1/* </b></FONT>"
+else
 rm -Rf /opt/mhvtl/$VAR1
+echo "<FONT COLOR=#FFA500><b>STATUS:$?</b></FONT>"
+echo "<pre><FONT COLOR=#FFFF00><b>Removed /opt/mhvtl/$VAR1 </b></FONT>"
+fi
+
 else
 grep ^Slot /etc/mhvtl/library_contents."$VAR1" | awk '{print $NF}'| cut -d ":" -f2 | grep -v ^$ | while read each; do
 rm -Rf /opt/mhvtl/$each
+echo "<FONT COLOR=#FFA500><b>STATUS:$?</b></FONT>"
+echo "<pre><FONT COLOR=#FFFF00><b>Removed /opt/mhvtl/$each </b></FONT>"
 done
 fi
 rm -f /etc/mhvtl/library_contents."$VAR1"
+echo "<FONT COLOR=#FFA500><b>STATUS:$?</b></FONT>"
 
 echo "<FONT COLOR=#FF0000><b>All existing tape media removed</b></FONT>"
 else
 echo "<FONT COLOR=#00FF00><b>Keeping all existing tape media ...</b></FONT>"
 rm -f /etc/mhvtl/library_contents."$VAR1"
+echo "<FONT COLOR=#FFA500><b>STATUS:$?</b></FONT>"
 fi
 
 
--- scripts/reset_default.sh
+++ scripts/reset_default.sh
@@ -8,8 +8,20 @@ fi
 /etc/init.d/mhvtl stop
 
 if [ "$1" = "YES" ]; then
-echo "<FONT COLOR=#FF0000><b>Removing all existing tape media ...</b></FONT>"
-sudo -u root -S rm -Rf /opt/mhvtl/*
+
+echo "<FONT COLOR=#FFFF00><b>Removing all existing tape media ...</b></FONT>"
+ls -d /opt/mhvtl/* | while read each; do
+if mountpoint -q -d $each ; then
+sudo -u root -S rm -Rf $each/*
+echo "<FONT COLOR=#FFA500><b>STATUS:$?</b></FONT>"
+echo "<FONT COLOR=#FF0000><b>Removed  $each/* </b></FONT>"
+else
+sudo -u root -S rm -Rf $each
+echo "<FONT COLOR=#FFA500><b>STATUS:$?</b></FONT>"
+echo "<FONT COLOR=#FF0000><b>Removed  $each </b></FONT>"
+fi
+done
+
 else
 echo "<FONT COLOR=#00FF00><b>Keeping all existing tape media ...</b></FONT>"
 fi
@@ -17,11 +29,17 @@ fi
 echo
 echo "<FONT COLOR=#FFFF00><b>Removing /etc/mhvtl/device.conf</b></FONT>"
 sudo -u root -S rm -f /etc/mhvtl/device.conf
+echo "<FONT COLOR=#FFA500><b>STATUS:$?</b></FONT>"
+echo "<FONT COLOR=#FF0000><b>Removed /etc/mhvtl/device.conf</b></FONT>"
 echo
 echo "<FONT COLOR=#FFFF00><b>Removing /etc/mhvtl/mhvtl.conf</b></FONT>"
 sudo -u root -S rm -f /etc/mhvtl/mhvtl.conf
+echo "<FONT COLOR=#FFA500><b>STATUS:$?</b></FONT>"
+echo "<FONT COLOR=#FF0000><b>Removed /etc/mhvtl/mhvtl.conf</b></FONT>"
 echo
 echo "<FONT COLOR=#FFFF00>Removing all /etc/mhvtl/library_contents.*</b></FONT>"
 sudo -u root -S rm -f /etc/mhvtl/library_contents.*
+echo "<FONT COLOR=#FFA500><b>STATUS:$?</b></FONT>"
+echo "<FONT COLOR=#FF0000><b>Removed /etc/mhvtl/library_contents.* </b></FONT>"
 echo
 echo done ..
