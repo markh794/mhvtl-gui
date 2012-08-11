ba9732fa147184a2b9de10e02c2ec590f50918e9
niadev67 <niadev67@gmail.com>
2012-08-10 23:01:17 -0400

    Updated SOFTWARE Disclaimer

--- html/about_console.php
+++ html/about_console.php
@@ -36,12 +36,14 @@ echo "<pre><b><FONT COLOR=purple >Web Console Release </FONT><FONT COLOR=black>$
 <pre>
 <FONT COLOR=blue size=4>SOFTWARE Disclaimer:</FONT>
 This SOFTWARE PRODUCT is provided by me "as is" and "with all faults." 
-I make no representations or warranties of any kind concerning the safety, suitability, lack of viruses, 
-inaccuracies, typographical errors, or other harmful components of this SOFTWARE PRODUCT. 
-There are inherent dangers in the use of any software, and you are solely responsible for determining 
-whether this SOFTWARE PRODUCT is compatible with your equipment and other software installed on your equipment. 
-You are also solely responsible for the protection of your equipment and backup of your data, and I will 
-not be liable for any damages you may suffer in connection with using, modifying, or distributing this SOFTWARE PRODUCT.
+I make no representations or warranties of any kind concerning the safety, 
+suitability, lack of viruses, inaccuracies, typographical errors, or other harmful
+components of this SOFTWARE PRODUCT. There are inherent dangers in the use
+of any software, and you are solely responsible for determining whether this SOFTWARE PRODUCT
+is compatible with your equipment and other software installed on your equipment.
+You are also solely responsible for the protection of your equipment and backup of your data,
+and I will not be liable for any damages you may suffer in connection with using,
+modifying, or distributing this SOFTWARE PRODUCT. 
 </FONT></pre>
 
 </table>
